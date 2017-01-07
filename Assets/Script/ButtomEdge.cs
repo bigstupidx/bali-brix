2b479851d0acd23e3556166874ce003623460de8
Soolan <info@soolan.com>
2017-01-07 12:46:51 +0800

    add assets folder

---
+++ Assets/Script/ButtomEdge.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtomEdge : MonoBehaviour
+{
+	public LevelManager levelManager;
+	// This is where a Collider object triggers
+	void OnTriggerEnter2D (Collider2D trigger)  // Type Collider2D
+	{
+		print ("Triggered");
+	}
+
+	// This is where A Collision object collides
+	void OnCollisionEnter2D (Collision2D collision) // Type Collision2D
+	{
+		print ("Collided");
+		levelManager.LoadLevel ("Win");
+	}
+}
