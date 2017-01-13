56824db8763cfed6d2bd249b2b195ac396493c8d
Soolan <info@soolan.com>
2017-01-13 11:41:47 +0800

    add progress bar

---
+++ Assets/Script/MyProgressBar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using ProgressBar;
+using System.Collections;
+using UnityEngine;
+
+public class example : MonoBehaviour
+{
+	ProgressBarBehaviour BarBehaviour;
+	[SerializeField] float UpdateDelay = 2f;
+
+	IEnumerator Start ()
+	{
+		BarBehaviour = GetComponent<ProgressBarBehaviour> ();
+		while (true) {
+			yield return new WaitForSeconds (UpdateDelay);
+			BarBehaviour.Value = Random.value * 100;
+			print ("new value: " + BarBehaviour.Value);
+		}
+	}
+}
