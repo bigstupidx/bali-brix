92fa5fdf3434bbbb6931ca3a009f49e13fc3a8c4
Soolan <info@soolan.com>
2017-01-25 12:53:44 +0800

    add level complete buttons

--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -15,7 +15,7 @@ public class LevelManager : MonoBehaviour
 	private GameObject starLeft, starMiddle, starRight;
 	private GameObject score, levelCompleteScore;
 
-	private float timeLeft = 15f;
+	private float timeLeft = 95f;
 	private GameObject timer;
 	private int totalBricks;
 	private string minsAndSecs = "0:0";
