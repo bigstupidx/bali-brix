77566e8e5228798a43708139e5327655efcb65e6
Soolan <info@soolan.com>
2017-01-31 20:20:03 +0800

    add new background sprites

---
+++ Assets/Script/Loose.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loose : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void LoadLevel (string name)
+	{
+		// Need to reset the brick counts otherwise the leftover from last game 
+		// will be added to the new game
+		Brick.brickCounts = 0;
+		Ball.hasStarted = false;
+		SceneManager.LoadScene (name);
+	}
+
+}
---
+++ Assets/Script/StartScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void Play ()
+	{
+		Brick.brickCounts = 0;
+		Ball.hasStarted = false;
+
+		//Ball.ballCounts = 3;
+		SceneManager.LoadScene ("Level Selection");
+	}
+
+	public void Quit ()
+	{
+		Debug.Log ("quit!");
+		Application.Quit ();
+	}
+}
