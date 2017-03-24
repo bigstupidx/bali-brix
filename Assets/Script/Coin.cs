9be5b4b954f0d2be50df61739f684c423bc51dea
Soolan <info@soolan.com>
2017-03-24 17:07:38 +0800

    add coin value

--- Assets/Script/Coin.cs
+++ Assets/Script/Coin.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Coin : MonoBehaviour
 {
-
+	public GameObject coinValue;
+	private int value;
 	// Use this for initialization
 	void Start ()
 	{
-		LevelManager.coins += 7;
+		value = Random.Range (4, 26);
+		LevelManager.coins += value;
+		ShowValue (value);
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	public void ShowValue (int value)
 	{
-		
+		GameObject g = 
+			Instantiate (coinValue, new Vector3 (0f, 0f, 0f), transform.rotation) as GameObject;
+		g.GetComponent <TextMesh> ().text = "Bada";
+		print ("coin value: " + value);
+		print ("coin value loc: " + g.transform.position);
+
 	}
 }
