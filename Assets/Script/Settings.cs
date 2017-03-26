936c8cbe7f3ae47c118d747aacdb26fcc077b967
Soolan <info@soolan.com>
2017-03-26 12:20:07 +0800

    add coin counter to the settings page

--- Assets/Script/Settings.cs
+++ Assets/Script/Settings.cs
@@ -6,19 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class Settings : MonoBehaviour
 {
-	public Sprite[] soundIcons;
-	private GameObject sound;
+	private GameObject coins;
 
 	void Start ()
 	{
-		sound = GameObject.Find ("Sound Settings");
-	}
-
-	public void toggleSound ()
-	{
-		AudioListener.pause = (AudioListener.pause) ? false : true;
-		AudioListener.volume = (AudioListener.pause) ? 0 : 2;
-		sound.GetComponent <Image> ().sprite = (AudioListener.pause) ? soundIcons [1] : soundIcons [0];
-		print ((AudioListener.pause) ? "paused" : "playing...");
+		coins = GameObject.Find ("count");
+		if (coins)
+			coins.GetComponent <Text> ().text = LevelManager.coins.ToString ();
 	}
 }
