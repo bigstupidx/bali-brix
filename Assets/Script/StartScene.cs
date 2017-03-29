8dc882b145caabab6d01607564aea93be017c0e0
Soolan <info@soolan.com>
2017-03-29 16:49:17 +0800

    daily rewards - wip

--- Assets/Script/StartScene.cs
+++ Assets/Script/StartScene.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
+using UnityEditor;
 
 public class StartScene : MonoBehaviour
 {
@@ -11,22 +12,52 @@ public class StartScene : MonoBehaviour
 	public Canvas dailyRewardCanvas;
 	public static bool rewarded = false;
 
+	private LevelManager levelManager;
+
 	void Start ()
 	{
 		helpCanvas.enabled = false;
 		settingsCanvas.enabled = false;
 		dailyRewardCanvas.enabled = false;
 
-		CheckDate ();
+		SetDailyReward (CheckDate ());
 	}
 
-	private void CheckDate ()
+	private void SetDailyReward (TimeSpan timeSpan)
+	{
+		if (timeSpan.Hours < 48 && timeSpan.Hours > 24) {
+			//ToDo: Pay the nextt reward
+			HandleNextReward ();
+		} else if (timeSpan.Hours >= 48) {
+			//ToDo: reset the reward to the day 1
+			ResetDailyRewards ();
+		}
+	}
+
+	private void HandleNextReward ()
+	{
+		
+	}
+
+	private void ResetRewards ()
+	{
+		
+	}
+
+	private TimeSpan CheckDate ()
 	{
 		//Store the current time when it starts
 		DateTime currentDate = System.DateTime.Now;
 
+		if (!PlayerPrefs.HasKey ("Last Visit")) {
+			// Happy Birthday Sohail
+			PlayerPrefs.SetString (
+				"Last Visit", 
+				Convert.ToDateTime ("3/21/1975 5:00:00 AM").ToBinary ().ToString ()
+			);
+		}
 		//Grab the old time from the player prefs as a long
-		long temp = Convert.ToInt64 (PlayerPrefs.GetString ("sysString"));
+		long temp = Convert.ToInt64 (PlayerPrefs.GetString ("Last Visit"));
 
 		//Convert the old time from binary to a DataTime variable
 		DateTime oldDate = DateTime.FromBinary (temp);
@@ -34,7 +65,7 @@ public class StartScene : MonoBehaviour
 
 		//Use the Subtract method and store the result as a timespan variable
 		TimeSpan difference = currentDate.Subtract (oldDate);
-		print ("Difference: " + difference);
+		return difference;
 	}
 
 	public void DailyRewards ()
@@ -70,7 +101,6 @@ public class StartScene : MonoBehaviour
 
 	public void Quit ()
 	{
-		Debug.Log ("quit!");
-		Application.Quit ();
+		levelManager.Quit ();
 	}
 }
