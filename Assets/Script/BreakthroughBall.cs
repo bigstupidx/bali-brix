36ec388dd92d76735f11b7f6cdd9145469146114
Soolan <info@soolan.com>
2017-04-03 18:45:51 +0800

    durian sprite - wip

--- Assets/Script/BreakthroughBall.cs
+++ Assets/Script/BreakthroughBall.cs
@@ -9,11 +9,15 @@ public class BreakthroughBall : MonoBehaviour
 	private float timeLeft = 10f;
 	private bool active = true;
 	private GameObject[] allAvailableBricks;
+	private GameObject[] ball;
+	public Sprite durian, normal;
 	private bool trigger = false;
 
 	// Use this for initialization
 	void Start ()
 	{
+		allAvailableBricks = GameObject.FindGameObjectsWithTag ("Breakable");
+		ball = GameObject.FindGameObjectsWithTag ("Ball");
 		ToggleTrigger ();
 	}
 
@@ -39,9 +43,16 @@ public class BreakthroughBall : MonoBehaviour
 	private void ToggleTrigger ()
 	{
 		trigger = (trigger) ? false : true;
-		allAvailableBricks = GameObject.FindGameObjectsWithTag ("Breakable");
 		for (int index = 0; index < allAvailableBricks.Length; index++) {
 			allAvailableBricks [index].GetComponent <BoxCollider2D> ().isTrigger = trigger;
 		}
+		if (ball.Length != 0)
+			foreach (GameObject g in ball) {
+				if (trigger) {
+					g.GetComponent <SpriteRenderer> ().sprite = durian;
+				} else {
+					g.GetComponent <SpriteRenderer> ().sprite = normal;
+				}
+			}
 	}
 }
